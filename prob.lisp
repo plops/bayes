155189b0ef513209fc6d712c8d1c5d575381c8a1
martin <martin@hop.kcl.ac.uk>
2011-04-30 10:04:48 +0100

    recreated bayes candy draw prediction

---
+++ prob.lisp
@@ -0,0 +1,36 @@
+(defvar *pack-probability* '(.1 .2 .4 .2 .1))
+(defvar *cherry-per-pack* '(1 .75 .5 .25 .0))
+
+(defun hypothesis-prob-unnormalized (i limes cherries)
+  (let* ((hi (elt *cherry-per-pack* i))
+	 (likelihood (* (expt (- 1 hi) limes)
+			(expt hi cherries))))
+    (* likelihood (elt *pack-probability* i))))
+
+(defun hypothesis-prob (i limes cherries)
+  (/ (hypothesis-prob-unnormalized i limes cherries)
+     (loop for i below (length *pack-probability*) sum
+	  (hypothesis-prob-unnormalized i limes cherries))))
+
+(progn 
+  (with-open-file (s "/dev/shm/candy.dat" :direction :output
+		    :if-exists :supersede
+		    :if-does-not-exist :create)
+   (loop for l upto 10 do
+	(format s "~d " l)
+	(loop for i below (length *pack-probability*) do
+	     (format s "~d " (hypothesis-prob i l 0)))
+	(terpri s)))
+  (with-open-file (p "/dev/shm/candy.gp" :direction :output
+		     :if-exists :supersede :if-does-not-exist :create)
+    (format p "set grid;
+plot \"/dev/shm/candy.dat\" u 1:2 w l, \"/dev/shm/candy.dat\" u 1:3 w l, \"/dev/shm/candy.dat\" u 1:4 w l, \"/dev/shm/candy.dat\" u 1:5 w l, \"/dev/shm/candy.dat\" u 1:6 w l; pause -1;"))
+  (sb-ext:run-program "/usr/bin/gnuplot" '("/dev/shm/candy.gp")))
+
+(defun next-candy-is-lime-prob (limes cherries)
+  (loop for i below (length *pack-probability*) sum
+       (let ((draw-lime-prob (- 1 (elt *cherry-per-pack* i))))
+	 (* draw-lime-prob (hypothesis-prob i limes cherries)))))
+
+(loop for i upto 10 collect
+ (next-candy-is-lime-prob i 0))
