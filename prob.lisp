c0ce9f9fc1aeb5c3dceee518af37089e43d4265e
martin <martin@hop.kcl.ac.uk>
2011-05-04 19:25:44 +0100

    change function name

--- prob.lisp
+++ prob.lisp
@@ -1,16 +1,17 @@
 (defvar *pack-probability* '(.1 .2 .4 .2 .1))
 (defvar *cherry-per-pack* '(1 .75 .5 .25 .0))
 
-(defun hypothesis-prob-unnormalized (i limes cherries)
+(defun hypothesis-posterior-prob-unnormalized (i limes cherries)
   (let* ((hi (elt *cherry-per-pack* i))
 	 (likelihood (* (expt (- 1 hi) limes)
-			(expt hi cherries))))
-    (* likelihood (elt *pack-probability* i))))
+			(expt hi cherries)))
+	 (hypothesis-prior (elt *pack-probability* i)))
+    (* likelihood hypothesis-prior)))
 
-(defun hypothesis-prob (i limes cherries)
-  (/ (hypothesis-prob-unnormalized i limes cherries)
+(defun hypothesis-posterior-prob (i limes cherries)
+  (/ (hypothesis-posterior-prob-unnormalized i limes cherries)
      (loop for i below (length *pack-probability*) sum
-	  (hypothesis-prob-unnormalized i limes cherries))))
+	  (hypothesis-posterior-prob-unnormalized i limes cherries))))
 
 (progn 
   (with-open-file (s "/dev/shm/candy.dat" :direction :output
@@ -19,7 +20,7 @@
    (loop for l upto 10 do
 	(format s "~d " l)
 	(loop for i below (length *pack-probability*) do
-	     (format s "~d " (hypothesis-prob i l 0)))
+	     (format s "~d " (hypothesis-posterior-prob i l 0)))
 	(terpri s)))
   (with-open-file (p "/dev/shm/candy.gp" :direction :output
 		     :if-exists :supersede :if-does-not-exist :create)
@@ -30,7 +31,7 @@ plot \"/dev/shm/candy.dat\" u 1:2 w l, \"/dev/shm/candy.dat\" u 1:3 w l, \"/dev/
 (defun next-candy-is-lime-prob (limes cherries)
   (loop for i below (length *pack-probability*) sum
        (let ((draw-lime-prob (- 1 (elt *cherry-per-pack* i))))
-	 (* draw-lime-prob (hypothesis-prob i limes cherries)))))
+	 (* draw-lime-prob (hypothesis-posterior-prob i limes cherries)))))
 
 (loop for i upto 10 collect
  (next-candy-is-lime-prob i 0))
